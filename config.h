f43e0f999924ba88b4d8cd8ed150c25ba6c1bf85
Srijan R Shetty <srijan.shetty@gmail.com>
2015-04-11 20:10:02 +0530

    Add DATAFILE and QUERYFILE

--- config.h
+++ config.h
@@ -2,6 +2,10 @@
 #define OUTPUT
 // #define TIME
 
+// Constants
+#define DATAFILE "assgn6_data_unif.txt"
+#define QUERYFILE "assgn6_querysample_unif.txt"
+
 // -- Auto Generated --
 #define BITS 2
 #define DIMENSIONS 25
